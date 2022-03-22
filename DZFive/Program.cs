19f41368d00d32d6a24183852ac8115c98b5abb4
[GeNrI16] <[Denisow16@yandex.ru]>
2022-03-22 20:01:04 +0500

    Добавьте файлы проекта.

---
+++ DZFive/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZFive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 0;
+            int c = 0;
+            Console.WriteLine("Введите начало диапазона");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите конец диапазона");
+            int b = int.Parse(Console.ReadLine());
+            while (a <= b)
+            {
+
+                if (a % 2 == 0)
+                {
+                    c++;
+                }
+
+                else
+                { d++; }
+                a++;
+            }
+            Console.WriteLine("четных чисел в диапазоне " + c);
+            Console.WriteLine("нечетных  чисел в диапазоне " + d);
+
+        }
+    }
+}
